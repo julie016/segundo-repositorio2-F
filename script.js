51d87584616c9033dc09735a77e2f51e1661c8b6
julie016 <lecheta.julie@escola.pr.gov.br>
2025-07-31 21:09:45 -0300

    aula8 - 31/07

---
+++ script.js
@@ -0,0 +1,52 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins.js";
+
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+
+function mostraPalavraChave() {
+    //alert("Fui clicado!");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+    //let palavras = texto.split(/\s+/);  "retirar espaços"
+    //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
+    let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    const frequencias = contaFrequencias(palavras);
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for ( let i of palavras) {
+        frequencias[i] = 0;
+        for ( let j of palavras) {
+            if( i == j ) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return palavras;
+}
+
+function tiraPalavrasRuins(palavras) {
+    //const PALAVRAS_RUINS = new setInterval(["para", "uma", "nós"]); //tem muito mais no arquivo JS importado
+    const palavrasBoas =[];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
